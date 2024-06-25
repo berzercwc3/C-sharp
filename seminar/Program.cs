696acb7d0b33da6d66492ac5c1fa4ff0b339fb96
SlavaShvets <slavashvets1@gmail.com>
2024-06-25 19:19:19 +0700

    Открыли документ

---
+++ seminar/Program.cs
@@ -0,0 +1,11 @@
+﻿int firstNumber = 9;
+int secondNumber = -3;
+
+if(firstNumber == secondNumber * secondNumber)
+{
+    System.Console.WriteLine("a = " + firstNumber + ", b = " + secondNumber);
+}
+else
+{
+    System.Console.WriteLine("No");
+}
