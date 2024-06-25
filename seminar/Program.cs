23857247adf1afdaa9a9a0bc0b53c835e3fdef67
SlavaShvets <slavashvets1@gmail.com>
2024-06-25 20:12:36 +0700

    Добавили новую задачу

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -1,11 +1,16 @@
-﻿int firstNumber = 9;
-int secondNumber = -3;
+﻿// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
 
-if(firstNumber == secondNumber * secondNumber)
+Console.Write("Введите положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = -number;
+while(i <= number)
 {
-    System.Console.WriteLine("a = " + firstNumber + ", b = " + secondNumber);
-}
-else
-{
-    System.Console.WriteLine("No");
+    System.Console.WriteLine(i + " ");
+    i++; // i = i + 1; // i += 1;
 }
